429f461b77c10787d120ab3952d64c53f625f16a
Jershz <joshuavasher@comcast.net>
2025-09-04 12:48:21 -0400

    Add getHumanChoice(), win(), lose(), playRound(), and score keeping

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,5 +1,41 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice () {
     let choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * 3)]
 }
 
+function getHumanChoice () {
+    let choice = prompt("Choose rock, paper, or scissors: ", "rock");
+    choice = choice.toLowerCase();
+    if(choice[0] == "r") return "rock";
+    else if(choice[0] == "p") return "paper";
+    else if(choice[0] == "s") return "scissors";
+    else return "rock";
+}
+
+function win() {
+    console.log("You win! +1 point to you.");
+    humanScore++;
+}
+
+function lose() {
+    console.log("You lose! +1 point to computer")
+    computerScore++;
+}
+
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice == computerChoice ) {
+        console.log("Tie! No points.");
+        console.log(humanChoice + " " + computerChoice);
+        return;  
+    }
+
+    let result = humanChoice == "rock" && computerChoice == "paper" ? lose() : 
+        humanChoice == "paper" && computerChoice == "scissors" ? lose() :
+        humanChoice == "scissors" && computerChoice == "rock" ? lose() : win();
+    console.log(humanChoice + " " + computerChoice);        
+}
+
+playRound(getHumanChoice(), getComputerChoice());
